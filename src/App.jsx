522e156f1d965428e8c10607b473ac2e5e88ec27
Anamaria Gherghisan <sanduanamaria44@yahoo.com>
2025-01-31 20:47:26 +0000

    pass repos as prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+import { Banner } from "./components/Banner";
+import { Repos } from "./components/Repos";
+import repos from "./data/repos.json";
+
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <main>
+      <Banner />
+      <Repos repos={repos} />
+    </main>
+  );
 };
--- src/components/Repos/index.jsx
+++ src/components/Repos/index.jsx
@@ -1,3 +1,16 @@
-export const Repos = () => {
-  return <div>Repos</div>;
+import { Alert } from "../Alert";
+import { Repo } from "../Repo";
+
+export const Repos = ({ repos }) => {
+  // check if repos length is 0 then render Alert
+  // map through repos and for each repo render the Repo component
+  return (
+    <section>
+      <Alert />
+      <Repo />
+      <Repo />
+      <Repo />
+      <Repo />
+    </section>
+  );
 };
